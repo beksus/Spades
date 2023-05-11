2a8df1f0b5aeb9177f9d88377780190179a93a63
beksus <85835904+beksus@users.noreply.github.com>
2023-05-11 20:47:40 +0800

    N\A
    nothing

--- Card.java
+++ Card.java
@@ -2,13 +2,4 @@ public abstract class Card{
     public final static int ACE = 1;
     public final static int JACK = 1;
     public final static int QUEEN = 1;
-    public final static int KING = 1;
-
-    private int val;
-
-    public Card(int val){
-        this.val = val;
-    }
-    public
-
 }
