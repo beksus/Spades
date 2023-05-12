07832054e27f6a25159bbeefc5ef6c176003efbd
beksus <85835904+beksus@users.noreply.github.com>
2023-05-12 09:11:15 +0800

    Update Diamonds.java

--- Diamonds.java
+++ Diamonds.java
@@ -1,6 +1,12 @@
 public class Diamonds extends Card{
+    /**
+     * @param val
+     */
     public Diamonds(int val){
 
+    }
+    public int GetCard() {
+        return super.GetCard();
     }
     public int CalcTrickBonus(){
         return 0;
