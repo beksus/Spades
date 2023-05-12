41e2145aef674913fee20fe5185d9d342f31b622
beksus <85835904+beksus@users.noreply.github.com>
2023-05-12 09:11:21 +0800

    Update Spades.java

--- Spades.java
+++ Spades.java
@@ -1,6 +1,12 @@
 public class Spades extends Card{
+    /**
+     * @param val
+     */
     public Spades(int val){
 
+    }
+    public int GetCard() {
+        return super.GetCard();
     }
     public int CalcTrickBonus(){
         return 0;
