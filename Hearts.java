79a04887573de6244efa1b17ef79420e6380cdba
beksus <85835904+beksus@users.noreply.github.com>
2023-05-12 09:11:18 +0800

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -1,6 +1,12 @@
 public class Hearts extends Card{
+    /**
+     * @param val
+     */
     public Hearts(int val){
 
+    }
+    public int GetCard() {
+        return super.GetCard();
     }
     public int CalcTrickBonus(){
         return 0;
