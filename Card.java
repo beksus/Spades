698435fd559fca64b187ec2716fe32246bda1c68
beksus <85835904+beksus@users.noreply.github.com>
2023-05-12 08:31:29 +0800

    Update Card.java
    miss spell

--- Card.java
+++ Card.java
@@ -2,7 +2,7 @@ public abstract class Card{
     public final static int ACE = 1;
     public final static int JACK = 1;
     public final static int QUEEN = 1;
-    public final static inf KING = 1;
+    public final static int KING = 1;
     private int val;
     public Card(int val){
 
