5bb6274d37d4d48a86405ed33e22d695395c9e93
beksus <85835904+beksus@users.noreply.github.com>
2023-05-12 09:11:12 +0800

    Update Clubs.java

--- Clubs.java
+++ Clubs.java
@@ -1,6 +1,12 @@
 public class Clubs extends Card{
+    /**
+     * @param val
+     */
     public Clubs(int val){
-        
+
+    }
+    public int GetCard() {
+        return super.GetCard();
     }
     public int CalcTrickBonus(){
         return 0;
