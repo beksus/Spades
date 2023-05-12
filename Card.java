8c773827754430d3051156421d5eb39f6a477661
beksus <85835904+beksus@users.noreply.github.com>
2023-05-12 08:31:06 +0800

    Update Card.java
    change in card adding abstact and other methods

--- Card.java
+++ Card.java
@@ -2,4 +2,15 @@ public abstract class Card{
     public final static int ACE = 1;
     public final static int JACK = 1;
     public final static int QUEEN = 1;
+    public final static inf KING = 1;
+    private int val;
+    public Card(int val){
+
+    }
+    public int GetCard(){
+        return 0;//change alter
+    }
+    public abstract int CalcTrickBonus(){
+        return 0;
+    }
 }
