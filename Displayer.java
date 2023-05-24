f00cf6169503700d49d9eddc109f5fd633e87bb3
beksus <85835904+beksus@users.noreply.github.com>
2023-05-24 13:05:21 +0800

    Update Displayer.java

--- Displayer.java
+++ Displayer.java
@@ -15,8 +15,18 @@ public class Displayer{
      * @param player
      */
     public void PrintScoreboard(Player[] player){
-        for(int i=0; i<player.length;i++){
-            System.out.println(player[i].GetScore());
+                
+        for(int i=0;i<player.length;i++){
+            System.out.print("-------------");
         }
+        System.out.print("-\n");
+        for(int i=0;i<player.length;i++){
+            System.out.printf("|%6d%6s",player[i].GetScore(),"");
+        }
+        System.out.print("|\n");
+        for(int i=0;i<player.length;i++){
+            System.out.print("-------------");
+        }
+        System.out.print("-");
     }
 }
