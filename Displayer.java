bf4ea750fa8103945a927dac0435028aa3596f05
beksus <85835904+beksus@users.noreply.github.com>
2023-05-25 12:39:02 +0800

    Update Displayer.java

--- Displayer.java
+++ Displayer.java
@@ -15,37 +15,42 @@ public class Displayer{
      * @param player
      */
     public void PrintScoreboard(Player[] player){
-        double n=3;
+        double n=3;// to have a dinamic displayer
         if(player.length>2&&player.length<4){
             n=4;
         }
         else if(player.length>3&&player.length<5){
             n=4.5;
         }
-        for(int i=0; i<player.length;i++){
+
+        for(int i=0; i<player.length;i++){//upper line for board
             System.out.print("------------");
         }
-        System.out.print("-\n");
-        System.out.print("|");
-        for(int i =0;i<player.length*n;i++){
+
+        System.out.print("-\n");// next line
+        System.out.print("|");//closing brackets for board
+
+        for(int i =0;i<player.length*n;i++){//printing the space to middle the name
             System.out.printf("%s"," ");
         }
         
-        System.out.print("SCORE BOARD");
+        System.out.print("SCORE BOARD");//printing the score board name
         
-        for(int i=0;i<player.length*n;i++){
+        for(int i=0;i<player.length*n;i++){//printing the space for backets to align
             System.out.printf("%s"," ");
         }
-        System.out.print("|\n");
-        for(int i=0;i<player.length;i++){
+        System.out.print("|\n");//printing the next line
+
+        for(int i=0;i<player.length;i++){//printing the middle line
             System.out.print("------------");
         }
-        System.out.print("-\n");
-        for(int i=0;i<player.length;i++){
+
+        System.out.print("-\n");//next line
+        for(int i=0;i<player.length;i++){//printing the score and aligning 
             System.out.printf("|%6d%5s",player[i].GetScore(),"");
         }
-        System.out.print("|\n");
-        for(int i=0;i<player.length;i++){
+        System.out.print("|\n");//next line
+        for(int i=0;i<player.length;i++){//printing the bottom line
             System.out.print("------------");
         }
         System.out.print("-");
