6f784ca8ff34c055fcb28cd6db67291b32ba8bc2
beksus <85835904+beksus@users.noreply.github.com>
2023-05-26 10:18:18 +0800

    Update TESTSS.java

--- TESTSS.java
+++ TESTSS.java
@@ -2,18 +2,23 @@
 public class TESTSS{
   public static void main(String[] args){
     int[] score = {6,4,4,4,8,9};
+    String[] type={"D","H","C","S","S","C"};
     for(int i=0;i<score.length;i++){
       System.out.print("------------");
     }
     System.out.print("-\n");
     for(int i=0;i<score.length;i++){
-      System.out.printf("|%d%10s",score[i]," ");
+      System.out.printf("|%d%s%9s",score[i],type[i]," ");
     }
     System.out.print("|\n");
     for(int i =0; i<score.length;i++){
       System.out.printf("|%11s"," ");
     }
     System.out.print("|\n");
+    for(int i=0;i<score.length;i++){
+      System.out.print("============");
+    }
+    System.out.print("=\n");
   }
   
   public static void PrintHand(Player player){
