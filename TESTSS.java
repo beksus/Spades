6f0f20d7c27a1dd7d0076dcb3aa17a2d20ffe99d
beksus <85835904+beksus@users.noreply.github.com>
2023-05-29 15:48:34 +0800

    Update TESTSS.java

--- TESTSS.java
+++ TESTSS.java
@@ -1,177 +1,66 @@
 
 public class TESTSS{
   public static void main(String[] args){
-    
-    //==========THE TRICKS===========
-    int[] players = {1,2,3,4};
-    String[] tricks = {"3H","4C","5D"};
-    for(int i=0;i<players.length;i++){
-      System.out.print("-------------");
-    }
-    System.out.print("-\n");
-    for(int i=0; i<=players.length;i++){
-      if(i<tricks.length){
-        System.out.printf("|%s%10s",tricks[i]," ");
-      }
-      else{
-        System.out.printf("|%12s"," ");
-      }
-    }
-    for(int j=0;j<4;j++){
-      System.out.print("\n");
-      for(int i =0; i<=players.length;i++){
-        System.out.printf("|%12s"," ");
-      }
-    }
-    
-    System.out.print("\n");
-    for(int i=0; i<=players.length;i++){
-      if(i<tricks.length){
-        System.out.printf("|%10s%s"," ",tricks[i]);
-      }
-      else{
-        System.out.printf("|%12s"," ");
-      }
-    }
-    System.out.print("\n");
-    for(int i=0;i<players.length;i++){
-      System.out.print("-------------");
-    }
-    
-    
-    
-    
-    
-    
-    System.out.println("\n\n");
-    //============ THE HAND===========
-    int[] score = {6,4,4,4,8,11};
-    String[] type={"D","H","C","S","S","C"};
-    for(int i=0;i<score.length;i++){
-      System.out.print("------------");
-    }
-    System.out.print("-\n");
-    
-    for(int i=0;i<score.length;i++){
-      if(score[i]==11){
-        System.out.printf("|J%s%9s",type[i]," ");
-      }
-      else if(score[i]==12){
-        System.out.printf("|Q%s%9s",type[i]," ");
-      }
-      else if(score[i]==13){
-        System.out.printf("|K%s%9s",type[i]," ");
-      }
-      else if(score[i]==14){
-        System.out.printf("|A%s%9s",type[i]," ");
-      }
-      else{
-        System.out.printf("|%d%s%9s",score[i],type[i]," ");
-      }
-    }
-    System.out.print("|\n");
-    for(int i =0; i<score.length;i++){
-      System.out.printf("|%11s"," ");
-    }
-    System.out.print("|\n");
-    for(int i=0;i<score.length;i++){
-      System.out.print("============");
-    }
-    System.out.print("=\n");
-  }
-  
-  public static void PrintHand(Player player){
-    
-    for(int i=0;i<player.GetHand().length;i++){
-      System.out.print("------------");
-    }
-    System.out.print("-\n");
-    for(int i=0;i<player.GetHand().length;i++){
-      System.out.printf("|%d%10s",player.GetHand()[i].GetVal()," ");
-    }
-    System.out.print("|\n");
-    for(int i =0; i<player.GetHand().length;i++){
-      System.out.printf("|%11s"," ");
-    }
-    System.out.print("|\n");
-  }
-  public void PrintTricks(Card[] trick){
-    char suit='S';
-    char val = 'A';
-    for(int i=0;i<trick.length;i++){
-      System.out.print("-------------");
-    }
-    System.out.print("-\n");
-    for(int i=0; i<=trick.length;i++){
-      if(i<trick.length){
-        
-        
-        
-        
-        
-        if(trick[i].GetVal()==Card.ACE){
-          suit='A';
+    int[] player = {1,2,4};
+    double n=3;// to have a dinamic displayer
+        if(player.length>2&&player.length<4){
+            n=6.3333333333333343788;
         }
-        else if(trick[i].GetVal()==Card.KING){
-          suit='K';
+        else if(player.length>3&&player.length<5){
+            n=6.8;
         }
-        else if(trick[i].GetVal()==Card.QUEEN){
-          suit='Q';
+
+        for(int i=0; i<player.length;i++){//upper line for board
+            System.out.print("-----------------");
         }
-        else if(trick[i].GetVal()==Card.JACK){
-          suit = 'J';
+
+        System.out.print("-\n");// next line
+        System.out.print("|");//closing brackets for board
+
+        for(int i =0;i<player.length*n;i++){//printing the space to middle the name
+            System.out.printf("%s"," ");
         }
         
+        System.out.print("SCORE BOARD");//printing the score board name
         
-        if(trick[i] instanceof Hearts){
-          val = 'H';
+        for(int i=0;i<player.length*n;i++){//printing the space for backets to align
+            System.out.printf("%s"," ");
         }
-        else if(trick[i] instanceof Diamonds){
-          val = 'D';
+        System.out.print("|\n");//printing the next line
+
+        for(int i=0;i<player.length;i++){//printing the middle line
+            System.out.print("-----------------");
         }
-        else if(trick[i] instanceof Spades){
-          val = 'S';
+
+        System.out.print("-\n");//next line
+        for(int i=0;i<player.length;i++){//printing the score and aligning 
+            System.out.printf("| Player:%3d%5s",player[i],"");
         }
-        else if(trick[i] instanceof Clubs){
-          val = 'C';
+        System.out.print("|\n");//next line
+        
+        for(int i=0;i<player.length;i++){//printing the score and aligning 
+            System.out.printf("| Bid:%6d%5s",player[i],"");
         }
+        System.out.print("|\n");//next line
         
-        if(trick[i].GetVal()>10){
-          System.out.printf("|%s%s%10s",trick[i],val," ");
+        for(int i=0;i<player.length;i++){//printing the score and aligning 
+            System.out.printf("| Tricks:%3d%5s",player[i],"");
         }
-        else{
-          System.out.printf("|%s%s%10s",trick[i],val," ");
+        System.out.print("|\n");//next line
+        
+        for(int i=0;i<player.length;i++){//printing the score and aligning 
+            System.out.printf("| Over Tricks:%2d%1s",player[i],"");
         }
+        System.out.print("|\n");//next line
         
+        for(int i=0;i<player.length;i++){//printing the score and aligning 
+            System.out.printf("| Score:%3d%6s",player[i],"");
+        }
+        System.out.print("|\n");//next line
         
-        
-        
-        
-        
-      }
-      else{
-        System.out.printf("|%12s"," ");
-      }
-    }
-    for(int j=0;j<4;j++){
-      System.out.print("\n");
-      for(int i =0; i<=trick.length;i++){
-        System.out.printf("|%12s"," ");
-      }
-    }
-    
-    System.out.print("\n");
-    for(int i=0; i<=trick.length;i++){
-      if(i<trick.length){
-        System.out.printf("|%10s%s"," ",trick[i]);
-      }
-      else{
-        System.out.printf("|%12s"," ");
-      }
-    }
-    System.out.print("\n");
-    for(int i=0;i<trick.length;i++){
-      System.out.print("-------------");
-    }
+        for(int i=0;i<player.length;i++){//printing the bottom line
+            System.out.print("-----------------");
+        }
+        System.out.print("-");
   }
 }
