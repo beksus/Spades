4e2e515d9f2670677d8300e4dc5feadff60ec2cc
beksus <85835904+beksus@users.noreply.github.com>
2023-05-29 08:57:08 +0800

    Update TESTSS.java

--- TESTSS.java
+++ TESTSS.java
@@ -1,6 +1,50 @@
 
 public class TESTSS{
   public static void main(String[] args){
+    
+    //==========THE TRICKS===========
+    int[] players = {1,2,3,4};
+    String[] tricks = {"3H","4C","5D"};
+    for(int i=0;i<players.length;i++){
+      System.out.print("-------------");
+    }
+    System.out.print("-\n");
+    for(int i=0; i<=players.length;i++){
+      if(i<tricks.length){
+        System.out.printf("|%s%10s",tricks[i]," ");
+      }
+      else{
+        System.out.printf("|%12s"," ");
+      }
+    }
+    for(int j=0;j<4;j++){
+      System.out.print("\n");
+      for(int i =0; i<=players.length;i++){
+        System.out.printf("|%12s"," ");
+      }
+    }
+    
+    System.out.print("\n");
+    for(int i=0; i<=players.length;i++){
+      if(i<tricks.length){
+        System.out.printf("|%10s%s"," ",tricks[i]);
+      }
+      else{
+        System.out.printf("|%12s"," ");
+      }
+    }
+    System.out.print("\n");
+    for(int i=0;i<players.length;i++){
+      System.out.print("-------------");
+    }
+    
+    
+    
+    
+    
+    
+    System.out.println("\n\n");
+    //============ THE HAND===========
     int[] score = {6,4,4,4,8,9};
     String[] type={"D","H","C","S","S","C"};
     for(int i=0;i<score.length;i++){
@@ -35,5 +79,51 @@ public class TESTSS{
       System.out.printf("|%11s"," ");
     }
     System.out.print("|\n");
+  }
+  public static void PrintTricks(Card[] tricks){
+    for(int i=0;i<tricks.length;i++){
+      System.out.print("-------------");
+    }
+    System.out.print("-\n");
+    for(int i=0; i<=tricks.length;i++){
+      if(i<tricks.length){
+        if(tricks[i] instanceof Hearts){
+          System.out.printf("|%s%s%10s",tricks[i],"H"," ");
+        }
+        else if(tricks[i] instanceof Diamonds){
+          System.out.printf("|%s%s%10s",tricks[i],"D"," ");
+        }
+        else if(tricks[i] instanceof Spades){
+          System.out.printf("|%s%s%10s",tricks[i],"S"," ");
+        }
+        else if(tricks[i] instanceof Clubs){
+          System.out.printf("|%s%s%10s",tricks[i],"C"," ");
+        }
+      }
+      else{
+        System.out.printf("|%12s"," ");
+      }
+    }
+    for(int j=0;j<4;j++){
+      System.out.print("\n");
+      for(int i =0; i<=tricks.length;i++){
+        System.out.printf("|%12s"," ");
+      }
+    }
+    
+    System.out.print("\n");
+    for(int i=0; i<=tricks.length;i++){
+      if(i<tricks.length){
+        System.out.printf("|%10s%s"," ",tricks[i]);
+      }
+      else{
+        System.out.printf("|%12s"," ");
+      }
+    }
+    System.out.print("\n");
+    for(int i=0;i<tricks.length;i++){
+      System.out.print("-------------");
+    }
+    
   }
 }
