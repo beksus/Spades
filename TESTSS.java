c7c2bdd84265c509c1f84c440830d1532976aebe
beksus <85835904+beksus@users.noreply.github.com>
2023-05-30 12:18:47 +0800

    Update TESTSS.java

--- TESTSS.java
+++ TESTSS.java
@@ -1,27 +1,27 @@
 
 public class TESTSS{
   public static void main(String[] args){
-    int[] player = {1,2,4};
-    double n=3;// to have a dinamic displayer
-        if(player.length>2&&player.length<4){
-            n=6.3333333333333343788;
-        }
-        else if(player.length>3&&player.length<5){
-            n=6.8;
-        }
+    int[] player = {1,2,3,3};
+    double n=5.5;// to have a dinamic displayer
+    if(player.length>2&&player.length<4){
+        n=6.3333333333333343788;
+    }
+    else if(player.length>3&&player.length<5){
+        n=6.8;
+    }
 
-        for(int i=0; i<player.length;i++){//upper line for board
-            System.out.print("-----------------");
-        }
+    for(int i=0; i<player.length;i++){//upper line for board
+        System.out.print("-----------------");
+    }
 
-        System.out.print("-\n");// next line
-        System.out.print("|");//closing brackets for board
+    System.out.print("-\n");// next line
+    System.out.print("|");//closing brackets for board
 
-        for(int i =0;i<player.length*n;i++){//printing the space to middle the name
-            System.out.printf("%s"," ");
-        }
+    for(int i =0;i<player.length*n;i++){//printing the space to middle the name
+        System.out.printf("%s"," ");
+    }
         
-        System.out.print("SCORE BOARD");//printing the score board name
+    System.out.print("SCORE BOARD");//printing the score board name
         
         for(int i=0;i<player.length*n;i++){//printing the space for backets to align
             System.out.printf("%s"," ");
@@ -49,7 +49,7 @@ public class TESTSS{
         System.out.print("|\n");//next line
         
         for(int i=0;i<player.length;i++){//printing the score and aligning 
-            System.out.printf("| Over Tricks:%2d%1s",player[i],"");
+            System.out.printf("| Overtricks:%2d%2s",player[i],"");
         }
         System.out.print("|\n");//next line
         
